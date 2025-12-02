9def009cceaf79a7ca8beb713c0577a9f9fbd88b
dr_uran <dr.uran@nxt.ru>
2025-12-02 22:45:21 +0300

    Add message root acess

--- utils/check_root.sh
+++ utils/check_root.sh
@@ -4,8 +4,8 @@
 ensure_root() {
     if [ "$(id -u)" -ne 0 ]; then
         if command -v sudo >/dev/null 2>&1; then
+            echo "======================================"
             echo "Необходимы права root. Попытка перезапуска с sudo..."
-            exec sudo -E bash "$0" "$@"
         else
             echo "Ошибка: нужны права root. Пожалуйста, запустите скрипт от root или установите sudo."
             exit 1
