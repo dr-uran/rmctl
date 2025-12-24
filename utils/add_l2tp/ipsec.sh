99d9fc93283d1e7cf489aac2962788804178efd2
dr_uran <dr.uran@nxt.ru>
2025-12-24 23:13:26 +0300

    Add l2tp/ipsec and routs

---
+++ utils/add_l2tp/ipsec.sh
@@ -0,0 +1,14 @@
+    nmcli connection add \
+    con-name "Goreltex-Master" \
+    type vpn \
+    vpn-type l2tp \
+    vpn.service-type org.freedesktop.NetworkManager.l2tp \
+    vpn.data \
+    "gateway=vpn.company.com,ipsec-enabled=yes,ipsec-psk=SHARED_SECRET_KEY,user=myusername,password-flags=0" \
+    vpn.secrets "password=mypassword123" \
+    ipv4.method auto \
+    ipv4.dns-search "company.local office.local intranet.corp" \
+    ipv4.routes "10.0.1.0/24 0.0.0.0,192.168.10.0/24 0.0.0.0,172.16.100.0/22 0.0.0.0" \
+    ipv4.ignore-auto-dns true \
+    ipv4.dns-priority 50 \
+    ipv6.method disabled
